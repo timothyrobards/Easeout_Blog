89e7fcb473e070340c9ffdf4592a1d7bbb0026d2
timothyrobards <32234220+timothyrobards@users.noreply.github.com>
2020-02-26 17:42:34 -0500

    Newsletter setup

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -97,7 +97,7 @@ export default {
         async processForm() {
             try {
                 const sendgrid = await this.$axios.post(
-                    `${process.env.API_URL}/.netlify/functions/sendgrid`,
+                    `${process.env.API_URL}/.netlify/functions/app`,
                     this.emaildata
                 );
                 console.log("Processed!");
