194cf308e2fcdd1ab5211b10b91a6448737522b6
timothyrobards <32234220+timothyrobards@users.noreply.github.com>
2020-03-24 14:07:16 -0400

    Add Sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // Enhance Nuxt's generate process by gathering all content files from Netifly CMS
 // automatically and match it to the path of your Nuxt routes.
-// The Nuxt routes are generate by Nuxt automatically based on the pages folder.
+// The Nuxt routes are generate by Nuxt automatically based on the pages folder..
 var dynamicRoutes = getDynamicPaths({
   '/blog': 'blog/posts/*.json',
   '/page': 'page/posts/*.json',
