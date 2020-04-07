f65ae659f3fee6065a0809338890b39835b4c43a
timothyrobards <32234220+timothyrobards@users.noreply.github.com>
2020-04-07 16:47:33 -0400

    no www's or trailing slashes - 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,8 +35,8 @@ env: {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Archivo+Black' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,900&display=swap"' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Overpass+Mono&display=swap"' }
-      // { rel: 'canonical', href: 'https://easeout.co' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Overpass+Mono&display=swap"' },
+      { rel: 'canonical', href: 'https://easeout.co' }
     ]
   },
   css: ["@/assets/grid.css","bf-solid/dist/solid.latest.css"],
