1cf7d2d77892344ae185674e3f929930fb28b1cb
Tim Robards <timothyrobards@gmail.com>
2024-01-30 03:42:46 +1100

    new changes

--- pages/freelance.vue
+++ pages/freelance.vue
@@ -100,8 +100,19 @@
           <div class="div7">
               <div class="center">
                 <h1 class="xs-py3 main-title freelance-heading" style="line-height:1.1;margin-top:-2em!important;">Start freelancing <span style="color:#B206FF;">today!</span></h1>
-                <p style="text-align:left!important;">Freelancing has opened up a world of opportunities for me, and I’m certain it can do the same for you. Get instant access to the Complete Guide to Freelancing and unlock the secrets to success.</p><br>
-                <a href="https://easeout.gumroad.com/l/freelance" target="_blank"><button>Get it now!</button></a>
+                <p>With this package, you'll gain the confidence and skills to:
+                  <ul>
+                  <li>✔️ Attract high-paying clients and projects</li>
+                  <li>✔️ Deliver top-notch work that exceeds expectations</li>
+                  <li>✔️ Streamline your workflow and maximize productivity</li>
+                  <li>✔️ row your freelancing business with a clear roadmap</li>
+                  </ul>
+
+                Don't let uncertainty hold you back from achieving your freelancing dreams. Get instant access to the <strong>Complete Guide to Freelancing</strong> and unlock the secrets to success! 
+
+                </p>
+                <p style="text-align:left!important;">Freelancing has opened up a world of opportunities for me, and I’m certain it can do the same for you.</p><br>
+                <a href="https://easeout.gumroad.com/l/freelance" target="_blank"><button>Get it now</button></a>
                 <br /><br />
               </div>
             
